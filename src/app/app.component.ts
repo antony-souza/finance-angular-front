78d47a4d0db0cd6b777d270d54dff0376a904aa2
antony-souza <antonygustavo09@hotmail.com>
2024-12-02 21:25:29 -0300

    Atualizando gráfico real time com WebSockets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChartService } from '../components/charts/services/chart.service';
-import { PieChartComponent } from '../components/pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HttpClientModule],
-  providers: [ChartService, PieChartComponent],
+  providers: [ChartService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/components/charts/charts.component.ts
+++ src/components/charts/charts.component.ts
@@ -1,61 +1,96 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 import { WebSocketService } from '../../web/socket.component'; // Seu serviço de WebSocket
-import { environment } from '../../environment/environment';
 import { ChartService } from './services/chart.service';
-import { PieChartComponent } from "../pie-chart/pie-chart.component";
+import { CommonModule } from '@angular/common';
 
-Chart.register(...registerables)
+Chart.register(...registerables);
 
 interface IChartInfo {
   labels: string[];
   data: number[];
+  type?: chartType;
 }
+
+type chartType = 'line' | 'bar' | 'pie' | 'doughnut' | 'radar' | 'polarArea' | 'bubble' | 'scatter';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
+  imports: [CommonModule],
   standalone: true,
-  styleUrl: './charts.component.scss',
-  imports: [PieChartComponent],
+  styleUrls: ['./charts.component.scss'],
 })
 export class ChartBaseComponent implements OnInit {
 
+  @ViewChild('barChart') barChartRef: ElementRef | undefined;
+
   public chart: any;
-  private chartInfo: any;
+  public chartInfo: any;
   private label: string[] = [];
   private data: number[] = [];
 
   constructor(
-    private readonly chartService: ChartService) {
-  }
+    private readonly chartService: ChartService,
+    private readonly webSocketService: WebSocketService
+  ) { }
 
   ngOnInit(): void {
     this.chartService.getChartInfo().subscribe((response) => {
       this.chartInfo = response;
-      console.log(response)
+      console.log(response);
       if (this.chartInfo != null) {
         for (let i = 0; i < this.chartInfo.length; i++) {
           this.label.push(this.chartInfo[i].label);
           this.data.push(this.chartInfo[i].data);
         }
-        this.createChart(this.label, this.data);
+
+        if (this.barChartRef && this.barChartRef.nativeElement) {
+          this.renderChart(this.barChartRef.nativeElement, 'bar');
+        }
+      }
+    });
+
+    this.webSocketService.on('update', data => {
+      console.log('Dados atualizados do WebSocket:', data);
+      this.chartInfo = data;
+      this.label = [];
+      this.data = [];
+
+      for (let i = 0; i < this.chartInfo.length; i++) {
+        this.label.push(this.chartInfo[i].label);
+        this.data.push(this.chartInfo[i].data);
+      }
+
+      if (this.barChartRef && this.barChartRef.nativeElement) {
+        this.renderChart(this.barChartRef.nativeElement, 'bar');
       }
     });
   }
 
-  createChart(labeldata: string[], realdata: any[]) {
-   /*  this.chart = new Chart('canvas', {
-      type: 'pie',
-      data: {
-        labels: labeldata,
-        datasets: [
-          {
-            label: 'No of sales',
-            data: realdata,
-          },
-        ],
-      },
-    }) */
+  private renderChart(canvasElement: HTMLCanvasElement, chartType: chartType) {
+    const ctx = canvasElement.getContext('2d');
+
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    if (ctx) {
+      this.chart = new Chart(ctx, {
+        type: chartType,
+        data: {
+          labels: this.label,
+          datasets: [
+            {
+              label: 'Vendas',
+              data: this.data,
+            },
+          ],
+        },
+        options: {
+          responsive: true
+        }
+      });
+    }
   }
 }
