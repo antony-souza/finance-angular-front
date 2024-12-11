6ecc843d311a31cc6e950b2362e51ce27e036654
antony-souza <antonygustavo09@hotmail.com>
2024-12-11 11:28:59 -0300

    Enviando form com File no update de funcionários

--- src/components/employees/dialog-put-employees/dialog-put-employees.component.ts
+++ src/components/employees/dialog-put-employees/dialog-put-employees.component.ts
@@ -17,31 +17,56 @@ interface IEmployeeUpdate {
   templateUrl: './dialog-put-employees.component.html',
   styleUrls: ['./dialog-put-employees.component.scss'],
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ...MATERIAL_COMPONENTS]
+  imports: [CommonModule, ReactiveFormsModule, ...MATERIAL_COMPONENTS],
 })
 export class DialogPutEmployeesComponent {
+
+  private selectedFile: File | null = null;
+
+  formUpdateEmployee = this.formBuilder.group({
+    name: [''],
+    email: ['', [Validators.email]],
+    image_url: new FormControl<string | Blob>(''),
+  });
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: { user_id: string },
     private readonly httpClient: HttpClient,
     private formBuilder: FormBuilder,
-    private dialogRef: MatDialogRef<DialogPutEmployeesComponent> 
-  ) { }
+    private dialogRef: MatDialogRef<DialogPutEmployeesComponent>
+  ) {}
 
-  formUpdateEmployee = this.formBuilder.group({
-    name: '',
-    email: ['', [Validators.email]],
-    image_url: new FormControl<string | Blob>(''),
-  });
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
 
   saveChanges() {
     if (this.formUpdateEmployee.valid) {
+      const formData = new FormData();
+
+      Object.entries(this.formUpdateEmployee.controls).forEach(([key, control]) => {
+        if (control.value) {
+          if (key === 'image_url' && this.selectedFile) {
+              formData.append(key, this.selectedFile);
+          } 
+            formData.append(key, control.value as string);
+        }
+      });
+
       this.httpClient
-        .put<IEmployeeUpdate>(`${environment.host}:${environment.port}/${environment.updateUser}/${this.data.user_id}`, this.formUpdateEmployee.value)
-        .subscribe(() => {
-          this.dialogRef.close(true);
+        .put<IEmployeeUpdate>(
+          `${environment.host}:${environment.port}/${environment.updateUser}/${this.data.user_id}`,
+          formData
+        )
+        .subscribe({
+          next: () => {
+            this.dialogRef.close(true);
+          }
         });
-    }
+    } 
   }
 
   closeDialog() {
